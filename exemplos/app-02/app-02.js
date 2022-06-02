11f3014a839f85d0e5bf79567833ec5ad5a63826
Robson Santana <roby87gyn@gmail.com>
2022-06-01 21:58:39 -0300

    subindo projetos e exercicios já feitos

---
+++ exemplos/app-02/app-02.js
@@ -0,0 +1,14 @@
+// Desenvolva um programa que leia dois numeros e imprima o maior
+
+let a = 10;
+let b = 100;
+
+let maior = 0;
+
+if (a > b) {
+    maior = a;
+} else {
+    maior = b;
+}
+
+console.log(maior)
